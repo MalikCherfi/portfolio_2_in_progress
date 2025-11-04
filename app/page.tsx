29ee6bd34c36ebde222096086d0663f5c7210502
malikcherfi <malik.cherfi@strateg.in>
2025-11-04 12:57:08 +0100

    🔨 Add reset button

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,28 @@
 
 import { Canvas } from "@react-three/fiber";
 import Scene from "../components/Scene";
+import { useState } from "react";
 
 export default function Home() {
+  const [resetCubePosition, setResetCubePosition] = useState(false);
+
   return (
     <div className="w-screen h-screen">
+      <div className="absolute top-5 left-5 z-10 flex gap-2">
+        <button onClick={() => setResetCubePosition(true)}>
+          Reset
+        </button>
+      </div>
       <Canvas
         shadows
         camera={{ position: [0, 1, 14], fov: 45 }}
         gl={{ antialias: true }}
         style={{ background: "#BED3C3" }}
       >
-        <Scene />
+        <Scene
+          resetCubePosition={resetCubePosition}
+          setResetCubePosition={setResetCubePosition}
+        />
       </Canvas>
     </div>
   );
