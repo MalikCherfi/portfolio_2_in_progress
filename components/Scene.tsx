6faf24dc7987df6fe05c2ccc513f2d614bbe6f27
malikcherfi <malik.cherfi@strateg.in>
2025-11-05 23:06:46 +0100

    🔨 Update spotLight parameters and shadox position

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,34 +1,29 @@
-import { Environment, ContactShadows } from "@react-three/drei";
+import { ContactShadows } from "@react-three/drei";
 import Cube from "./Cube";
 
 const Scene = () => {
   return (
     <>
-      <ambientLight intensity={0.35} />
+      <ambientLight intensity={0.7} />
 
       <spotLight
-        position={[0, 5, 0]}
-        angle={0.6}
+        position={[0, 5, 5]}
+        angle={1}
         penumbra={1}
-        intensity={0.3}
+        intensity={200}
         castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-radius={8}
       />
 
       <Cube />
 
       <ContactShadows
-        position={[0, -3.6, 0]}
+        position={[0, -5.6, 0]}
         opacity={0.7}
         width={1.4}
         height={1.4}
         blur={4}
         far={15}
       />
-
-      <Environment preset="studio" background={false} />
     </>
   );
 };
