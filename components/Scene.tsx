8ee08b522ddcc83bbdd8ac4fd5a24f9966cbc562
malikcherfi <malik.cherfi@strateg.in>
2025-11-13 16:01:47 +0100

    🔨 Add bounce

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,7 +1,10 @@
 import { ContactShadows } from "@react-three/drei";
 import Cube from "./Cube";
+import { useCubeStore } from "@/stores/cubeStore";
 
 const Scene = () => {
+  const bounceY = useCubeStore((state) => state.bounceY);
+
   return (
     <>
       <ambientLight intensity={0.7} />
@@ -19,8 +22,8 @@ const Scene = () => {
       <ContactShadows
         position={[0, -5.6, 0]}
         opacity={0.7}
-        width={1.4}
-        height={1.4}
+        width={1.4 + bounceY}
+        height={1.4 + bounceY}
         blur={4}
         far={15}
       />
