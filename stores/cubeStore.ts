73005a9577c2841aa976009cf45c4ff57b9728eb
malikcherfi <malik.cherfi@strateg.in>
2025-11-13 16:02:00 +0100

    🔨 Add bounce parameters

--- stores/cubeStore.ts
+++ stores/cubeStore.ts
@@ -3,9 +3,14 @@ import { create } from "zustand";
 type CubeStore = {
   reset: boolean;
   setReset: (value: boolean) => void;
+  bounceY: number;
+  setBounceY: (y: number) => void;
 };
 
 export const useCubeStore = create<CubeStore>((set) => ({
   reset: false,
   setReset: (value) => set({ reset: value }),
+
+  bounceY: 0,
+  setBounceY: (y: number) => set({ bounceY: y }),
 }));
