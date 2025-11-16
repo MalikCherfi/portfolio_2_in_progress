1103ef2f86e7e7db1ea033c625a6adc6f559e459
malikcherfi <malik.cherfi@strateg.in>
2025-11-17 00:11:54 +0100

    Update lights

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -6,26 +6,36 @@ import * as THREE from "three";
 const Scene = () => {
   const bounceY = useCubeStore((state) => state.bounceY);
 
-  const rightLight = new THREE.RectAreaLight("#ffffff", 2.0, 2, 2);
-  rightLight.position.set(2, 0, 7);
+  // Panneau lumineux à droite
+  const rightLight = new THREE.RectAreaLight("#ffffff", 3, 2, 2);
+  rightLight.position.set(2.5, 1, 7);
   rightLight.lookAt(0, 0, 0);
+
+  // Panneau lumineux derrière
+  const backLight = new THREE.RectAreaLight("#ffffff", 1.5, 3, 2);
+  backLight.position.set(0, 1, -6);
+  backLight.lookAt(0, 0, 0);
 
   return (
     <>
-      {/* Lumière d'ambiance, faible, juste pour déboucher les ombres */}
       <ambientLight intensity={0.25} color="#ffffff" />
 
-      {/* Lumière principale type “softbox” au-dessus du cube */}
-      <directionalLight
-        position={[0, 5, 0]} // pile au-dessus
-        intensity={1.2}
-        color="#ffffff"
+      <hemisphereLight
+        color={"#ffffff"}
+        groundColor={"#ffffff"}
+        intensity={0.15}
       />
+
+      <directionalLight position={[0, 5, 0]} intensity={1.1} color="#ffffff" />
+
       <primitive object={rightLight} />
+      <primitive object={backLight} />
+
       <Cube />
+
       <ContactShadows
         position={[0, -5.6, 0]}
-        opacity={0.7}
+        opacity={0.6}
         width={1.4 + bounceY}
         height={1.4 + bounceY}
         blur={4}
