721737a413d17520637620f40f24a151156aa180
malikcherfi <malik.cherfi@strateg.in>
2025-11-16 23:54:39 +0100

    Update light

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,23 +1,27 @@
 import { ContactShadows } from "@react-three/drei";
 import Cube from "./Cube";
 import { useCubeStore } from "@/stores/cubeStore";
+import * as THREE from "three";
 
 const Scene = () => {
   const bounceY = useCubeStore((state) => state.bounceY);
 
+  const rightLight = new THREE.RectAreaLight("#ffffff", 2.0, 2, 2);
+  rightLight.position.set(2, 0, 7);
+  rightLight.lookAt(0, 0, 0);
+
   return (
     <>
-      <ambientLight intensity={1} color={"#ffffff"} />
+      {/* Lumière d'ambiance, faible, juste pour déboucher les ombres */}
+      <ambientLight intensity={0.25} color="#ffffff" />
+
+      {/* Lumière principale type “softbox” au-dessus du cube */}
       <directionalLight
-        position={[5, 5, 5]}
-        intensity={1.0}
-        color={"#ffffff"}
+        position={[0, 5, 0]} // pile au-dessus
+        intensity={1.2}
+        color="#ffffff"
       />
-      <directionalLight
-        position={[-5, 2, -5]}
-        intensity={1}
-        color={"#ffffff"}
-      />
+      <primitive object={rightLight} />
       <Cube />
       <ContactShadows
         position={[0, -5.6, 0]}
