35e54254552577ce4ec3c981fbb9a1757b983708
malikcherfi <malik.cherfi@strateg.in>
2025-11-18 02:01:14 +0100

    update geometry position

--- components/BackgroundGeometry.tsx
+++ components/BackgroundGeometry.tsx
@@ -5,18 +5,33 @@ import { Float } from "@react-three/drei";
 
 type ItemType = "cube" | "sphere";
 
+type arrayItem = {
+  position: [number, number, number];
+  scale: number;
+  color: string;
+  type: ItemType;
+};
+
 const BackgroundGeometry = () => {
   const [items] = useState(() => {
-    const arr = [];
+    const arr: arrayItem[] = [];
     for (let i = 0; i < 160; i++) {
+      // Génère un Z aléatoire mais évite le cube principal (-2.6 à 2.6)
+      let z = 0;
+      if (Math.random() > 0.1) {
+        // Derrière le cube
+        z = -3 - Math.random() * 8; // entre -3 et -11
+      } else {
+        // Devant le cube
+        z = 3 + Math.random() * 8; // entre 3 et 11
+      }
+
       arr.push({
         position: [
-          (Math.random() - 0.5) * 20, // X normal
-          (Math.random() - 0.5) * 20, // Y normal
-
-          // Z forcé en arrière-plan uniquement
-          -3 - Math.random() * 8, // entre -3 et -11
-        ] as [number, number, number],
+          (Math.random() - 0.5) * 20, // X
+          (Math.random() - 0.5) * 20, // Y
+          z, // Z
+        ],
         scale: Math.random() * 0.4 + 0.1,
         color: Math.random() > 0.5 ? "#B6465F" : "#CE6A6B",
         type: Math.random() > 0.7 ? "sphere" : "cube",
@@ -25,7 +40,7 @@ const BackgroundGeometry = () => {
     return arr;
   });
 
-  const setGeometry: Record<ItemType, { component: ReactElement }> = {
+  const geometry: Record<ItemType, { component: ReactElement }> = {
     cube: { component: <boxGeometry args={[1, 1, 1]} /> },
     sphere: { component: <sphereGeometry args={[0.7, 16, 16]} /> },
   };
@@ -40,7 +55,7 @@ const BackgroundGeometry = () => {
           floatIntensity={0.5}
         >
           <mesh position={item.position} scale={item.scale}>
-            {setGeometry[item.type as ItemType].component}
+            {geometry[item.type as ItemType].component}
             <meshStandardMaterial
               color={item.color}
               emissive={item.color}
