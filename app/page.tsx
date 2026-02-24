e9988c56136f7869580e2d7dd834f09124c12f86
malikcherfi <malikcherfi@gmail.com>
2026-02-24 16:42:12 +0100

    Update isTextClicked value

--- app/page.tsx
+++ app/page.tsx
@@ -30,8 +30,8 @@ export default function Home() {
         {zoomCamera && (
           <button
             onClick={() => {
-              if (isTextClicked) {
-                setIsTextClicked(false);
+              if (isTextClicked.clicked) {
+                setIsTextClicked(false, "");
                 setZoomDone(true);
                 return;
               }
