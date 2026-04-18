e107ff0c6e8acb021c689a1cb6ac66d4b5dabe82
malikcherfi <malikcherfi@gmail.com>
2026-04-18 19:01:33 +0200

    refacto : add Buttons component

--- app/page.tsx
+++ app/page.tsx
@@ -2,53 +2,14 @@
 
 import { Canvas } from "@react-three/fiber";
 import Scene from "../components/Scene";
-import { useCubeStore } from "@/stores/cubeStore";
-import { RotateCcw, ArrowLeft } from "lucide-react";
+import { Buttons } from "../components/Buttons";
 import * as THREE from "three";
 import { Stats } from "@react-three/drei";
 
 export default function Home() {
-  const setRotate = useCubeStore((state) => state.setRotate);
-  const zoomCamera = useCubeStore((state) => state.zoomCamera);
-  const setZoomCamera = useCubeStore((state) => state.setZoomCamera);
-  const setZoomDone = useCubeStore((state) => state.setZoomDone);
-  const isTextClicked = useCubeStore((state) => state.isTextClicked);
-  const setIsTextClicked = useCubeStore((state) => state.setIsTextClicked);
-
   return (
     <div className="w-screen h-screen bg-gradient-to-t from-[#CE6A6B] to-[#EBACA2]">
-      <div className="absolute top-5 left-5 z-10 flex gap-2">
-        {!zoomCamera && (
-          <button
-            onClick={() => setRotate({ reset: true, target_face: false })}
-            className="p-3 rounded-full bg-white/70 hover:bg-white shadow-md"
-          >
-            <RotateCcw size={22} />
-          </button>
-        )}
-
-        {zoomCamera && (
-          <button
-            onClick={() => {
-              if (isTextClicked.clicked) {
-                setIsTextClicked(false, undefined);
-                setZoomDone(true);
-                return;
-              }
-
-              setZoomDone(false);
-
-              setTimeout(() => {
-                setZoomCamera(false);
-              }, 1000);
-            }}
-            className="p-3 rounded-full bg-white/70 hover:bg-white shadow-md"
-          >
-            <ArrowLeft size={22} />
-          </button>
-        )}
-      </div>
-
+      <Buttons />
       <Canvas
         style={{ touchAction: "none" }}
         shadows={false}
