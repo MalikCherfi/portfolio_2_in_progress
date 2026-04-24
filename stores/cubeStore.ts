656a3237d51682850db5300285378c04aa0514b0
malikcherfi <malikcherfi@gmail.com>
2026-04-24 14:42:12 +0200

    remove state to put in themeStore

--- stores/cubeStore.ts
+++ stores/cubeStore.ts
@@ -28,10 +28,6 @@ type CubeStore = {
     id: ProjectId | undefined;
   };
   setIsTextClicked: (clicked: boolean, id?: ProjectId) => void;
-  cubeColor: string;
-  setCubeColor: (color: string) => void;
-  geoColors: string[];
-  setGeoColors: (colors: string[]) => void;
 };
 
 export const useCubeStore = create<CubeStore>((set) => ({
@@ -50,10 +46,4 @@ export const useCubeStore = create<CubeStore>((set) => ({
   isTextClicked: { clicked: false, id: undefined },
   setIsTextClicked: (clicked, id) =>
     set({ isTextClicked: { clicked, id: id || undefined } }),
-
-  cubeColor: "#B6465F",
-  setCubeColor: (color) => set({ cubeColor: color }),
-
-  geoColors: ["#B6465F", "#CE6A6B"],
-  setGeoColors: (colors) => set({ geoColors: colors }),
 }));
